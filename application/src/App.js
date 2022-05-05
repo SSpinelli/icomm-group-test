677715f85b11aa7404aa1154a534977827d96986
joao <jgsspinelli92@gmail.com>
2022-05-05 20:00:14 -0300

    [Update] Cria o contexto global, e prepara o App.js para utiliza-lo

--- application/src/App.js
+++ application/src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Banner from './components/Banner';
 import Header from './components/Header';
 import Highlights from './components/Highlights';
+import myContext from './context';
 
 function App() {
+  const [favsItens, setFavsItens] = useState([]);
+  const [cartItens, setCartItens] = useState([]);
+
+  const contextValue = {
+    favsItens,
+    setFavsItens,
+    cartItens,
+    setCartItens
+  }
   return (
     <>
-      <Header />
-      <Banner />
-      <Highlights />
+      <myContext.Provider value={ contextValue }>
+        <Header />
+        <Banner />
+        <Highlights />
+      </myContext.Provider>
     </>
   );
 }
