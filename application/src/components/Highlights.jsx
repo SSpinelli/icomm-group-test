a17e3be32fbe7f7ab4bf639c659fa4204759d05b
joao <jgsspinelli92@gmail.com>
2022-05-05 02:55:14 -0300

    [UPDATE] Criação do componente Highlights e estilo

---
+++ application/src/components/Highlights.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './style/highlights.css'
+import SimpleSlider from "./SimpleSlider";
+
+const Highlights = (_props) => {
+  return (
+    <main>
+      <h1>Destaques</h1>
+      <SimpleSlider />
+    </main>
+  )
+}
+
+export default Highlights;
