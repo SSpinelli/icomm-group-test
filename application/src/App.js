436672e6b58556ded0a11140c0f66af8a746ffe2
joao <jgsspinelli92@gmail.com>
2022-05-05 02:53:34 -0300

    [Start] Arquivos iniciais REACT

---
+++ application/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Highlights from './components/Highlights';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Highlights />
+    </>
+  );
+}
+
+export default App;
