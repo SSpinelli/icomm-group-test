147297b78deedb4be34b94f5cefc0e7e24455d07
joao <jgsspinelli92@gmail.com>
2022-05-05 19:58:55 -0300

    [Update] Atualiza header, para o carrinho ficar dinâmico

--- application/src/components/Header.jsx
+++ application/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
 import './style/header.css';
 import magentoLogo from '../images/Magento.svg';
 import cart from '../images/Group 11.svg'
+import myContext from '../context'
 
 const Header = (_props) => {
+  const value = useContext(myContext)
   return (
     <header>
       <nav>
@@ -11,7 +13,12 @@ const Header = (_props) => {
           <img src={magentoLogo} alt="sss" />
           <h2>MCX</h2>
         </div>
-        <div><img src={cart} alt="ss" /></div>
+        <div className="container-cart">
+          <img src={cart} alt="icone de carrinho de compras" />
+          <div className="quantity-cart">
+            <span>{value.cartItens.length}</span>
+          </div>
+        </div>
       </nav>
     </header>
   )
