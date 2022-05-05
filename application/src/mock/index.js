889e77cc4132ffffd758a185067b4f26b01886f0
joao <jgsspinelli92@gmail.com>
2022-05-05 02:56:05 -0300

    [UPDATE] Criação do mock

---
+++ application/src/mock/index.js
@@ -0,0 +1,56 @@
+import foto1 from '../images/image 1.png';
+import foto2 from '../images/image 2.png';
+import foto3 from '../images/image 3.png';
+
+const mock = [
+  {
+    tag: "verão 2022",
+    title: "Macacão feminino Maria Filó sobreposição",
+    price: 759.00,
+    url: foto1
+  },
+  {
+    tag: "20% Off",
+    title: "Regata feminina de tricot com ombreira",
+    price: 759.00,
+    url: foto2,
+  },
+  {
+    tag: "",
+    title: "Blusa feminina silk merci",
+    price: 759.00,
+    url: foto3,
+  },
+  {
+    tag: "Lançamento",
+    title: "Blazer feminino liocel punho",
+    price: 759.00,
+    url: foto1
+  },
+  {
+    tag: "verão 2022",
+    title: "Macacão feminino Maria Filó sobreposição",
+    price: 759.00,
+    url: foto1
+  },
+  {
+    tag: "20% Off",
+    title: "Regata feminina de tricot com ombreira",
+    price: 759.00,
+    url: foto2,
+  },
+  {
+    tag: "",
+    title: "Blusa feminina silk merci",
+    price: 759.00,
+    url: foto3,
+  },
+  {
+    tag: "Lançamento",
+    title: "Blazer feminino liocel punho",
+    price: 759.00,
+    url: foto1
+  }
+]
+
+export default mock;
