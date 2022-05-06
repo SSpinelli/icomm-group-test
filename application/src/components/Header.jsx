c2a202d659a02f612fe137ca29ef4775355d44c3
joao <jgsspinelli92@gmail.com>
2022-05-06 13:34:18 -0300

    [TESTE] Muda a logo para svg

--- application/src/components/Header.jsx
+++ application/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import './style/header.css';
-import magentoLogo from '../images/Group 9.png';
+import magentoLogo from '../images/Magento.svg';
 import cart from '../images/Group 11.svg'
 import myContext from '../context'
 
@@ -11,6 +11,7 @@ const Header = (_props) => {
       <nav>
         <div className="logo-container">
           <img src={magentoLogo} alt="sss" />
+          <h2>MCX</h2>
         </div>
         <div className="container-cart">
           <img src={cart} alt="icone de carrinho de compras" />
