297721d0d4fb7723610cb510bca8540d4c941a7c
joao <jgsspinelli92@gmail.com>
2022-05-05 23:18:30 -0300

    Revert "[Update] Atualiza slider"
    This reverts commit 53399c4429f34df057cddf9e9bb3b6fb96b9eb66.

--- application/src/components/SimpleSlider.jsx
+++ application/src/components/SimpleSlider.jsx
@@ -1,114 +1,30 @@
-import React from "react";
-import Slider from "react-slick";
 import mock from '../mock'
+import React, { Component } from "react";
+import Slider from "react-slick";
 import SliderCard from './SliderCard';
-import leftArrow from '../images/Vector (6).svg'
-import rightArrow from '../images/Vector (5).svg';
-import './style/simpleSlider.css'
-import './style/arrow.css'
 
-export default class PreviousNextMethods extends React.Component {
-  constructor(props) {
-    super(props);
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.clickDot = this.clickDot.bind(this);
-    this.state = {
-      countSlider: 0,
-    }
-  }
-
-  next() {
-    const { countSlider } = this.state;
-    this.setState({countSlider: countSlider + 1}, this.slider.slickNext())
-  }
-  previous() {
-    const { countSlider } = this.state;
-    this.setState({countSlider: countSlider - 1}, this.slider.slickPrev())
-  }
-
-  clickDot(event) {
-    const allDots = document.querySelectorAll('.dots h1')
-    allDots.forEach((dot, index) => {
-      if (index === Number(event.target.id)) {
-        dot.classList.add('ativo')
-        dot.classList.remove('inativo')
-      } else {
-        dot.classList.remove('ativo')
-        dot.classList.add('inativo')
-      }
-    })
-  }
-
+export default class SwipeToSlide extends Component {
   render() {
-    const {countSlider} = this.state;
     const settings = {
       className: "center",
-      dots: false,
+      dots: true,
       infinite: true,
       centerPadding: "60px",
       slidesToShow: 4,
       swipeToSlide: true,
       afterChange: function(index) {
-        return index + 1
-      },
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 414,
-          settings: {
-            slidesToShow: 1.5,
-            slidesToScroll: 1,
-            infinite: false,
-            dots: true,
-            appendDots: dots => (
-              <div
-                style={{
-                  backgroundColor: "#fff",
-                  marginTop: "50px"
-                }}
-              >
-                <ul style={{ margin: "-15px", display: 'flex', justifyContent: 'center', alignItems: 'center' }}> {dots} </ul>
-              </div>
-            ),
-            customPaging: (i) => (
-              <div className="dots">
-                <h1 id={i} className={i === 0 ? 'ativo' : 'inativo'} onClick={this.clickDot}>•</h1>
-              </div>
-            ),
-          }
-        }
-      ]
+        console.log(
+          `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+        );
+      }
     };
     return (
-      <div className='slider-container'>
-        <Slider className="slider2" ref={c => (this.slider = c)} {...settings}>
-          { mock.map((i, index) => (
-            <SliderCard key={index} card={i} />
-          )) }
-        </Slider>
-        <button disabled={countSlider === 0} className={`button ${countSlider === 0 ? 'last-slide-left' : 'arrow-left'}`}onClick={this.previous}>
-          <img src={leftArrow} alt="botão de voltar" />
-        </button>
-        <button disabled={countSlider === mock.length - 4} className={`button ${countSlider === mock.length - 4 ? 'last-slide-right' : 'arrow-right'}`} onClick={this.next}>
-          <img src={rightArrow} alt="botão de passar" />
-        </button>
+      <div>
+        <Slider {...settings}>
+        { mock.map((i, index) => (
+          <SliderCard key={index} card={i} />
+        )) }
+      </Slider>
       </div>
     );
   }
